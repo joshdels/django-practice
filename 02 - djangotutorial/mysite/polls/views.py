c2e3d0e0a3e587012753c04b61c014eaf4529cc7
joshdels <deleonjoshy2@gmail.com>
2025-07-13 21:29:21 +0800

    update

--- 02 - djangotutorial/mysite/polls/views.py
+++ 02 - djangotutorial/mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -13,15 +14,15 @@ class IndexView(generic.ListView):
   context_object_name = "latest_question_list" 
     
   def get_queryset(self):
-    return Question.objects.order_by("-pub_date")[:5]
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("pub_date")[:5]
 
 class DetailView(generic.DetailView):
   model = Question
-  template_name = "polls/details.html"
+  template_name = "polls/detail.html"
 
 class ResultsView(generic.DetailView):
   model = Question
-  template_name = "polls/results.html"
+  template_name = "polls/result.html"
   
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
@@ -30,7 +31,7 @@ def vote(request, question_id):
   except (KeyError, Choice.DoesNotExist):
     return render(
       request,
-      "polls/details.html",
+      "polls/detail.html",
       {
         "question": question,
         "error_message": "You didn't select a choice.",
@@ -39,4 +40,4 @@ def vote(request, question_id):
   else:
     select_choice.votes = F("votes") + 1
     select_choice.save()
-    return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
