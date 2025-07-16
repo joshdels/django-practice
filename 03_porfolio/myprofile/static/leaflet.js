3f428a697a06c4184a396e9c4753112bb1f0f7b6
joshdels <deleonjoshy2@gmail.com>
2025-07-16 22:00:25 +0800

    update

--- 03_porfolio/myprofile/static/leaflet.js
+++ 03_porfolio/myprofile/static/leaflet.js
@@ -1,8 +1,30 @@
 let map = L.map('map', {
   zoomControl: false
-}).setView([13.41, 122.56], 10);
+}).setView([13.41, 122.56], 5);
 
-let myLocation = L.marker([7.442619, 125.804142]).addTo(map);
+var myFAIcon = L.divIcon({
+    html: '<i class="fas fa-map-pin fa-3x" style="color: red;"></i>',
+    iconSize: [25, 41],   // approximate marker size
+    iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -40], // where the popup will open relative to the iconAnchor
+    className: 'my-fa-icon' // optional class to style further
+});
+
+let myLocation = L.marker(
+  [7.442619, 125.804142], 
+  {icon: myFAIcon}
+).addTo(map);
+
+myLocation.bindPopup(`
+  <div style="line-height: 1.4; font-size: 14px;">
+    <h5 style="margin: 0 0 8px;">🏠 My Hometown</h5>
+    <p style="margin: 4px 0;">
+      <i class="fas fa-tree" style="color: green;"></i> Peaceful rural-style city<br>
+      <i class="fas fa-wallet" style="color: #5bc0de;"></i> Low cost of living<br>
+      <i class="fas fa-coffee" style="color: #795548;"></i> I'm open for a coffee talk!
+    </p>
+  </div>
+`).openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 11,
